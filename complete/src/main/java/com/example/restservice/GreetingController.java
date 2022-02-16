c8a5cb50ec2c76fc9a9705c0d099280cfe9fb772
stephenZhu2021 <zhuxciwei@gmail.com>
2022-02-16 18:03:21 -0500

    exercise

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -2,9 +2,7 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -15,5 +13,18 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/person")
+	public Person getPerson(@RequestParam(value = "fName") String firstName,@RequestParam(value = "lName") String lastName){
+
+		return new Person(firstName,lastName);
+	}
+
+	@PostMapping("/person")
+//	@RequestMapping(value="/person",method=RequestMethod.Post)
+	public String createPerson(@RequestBody Person person){
+		System.out.println(person);
+		return "person is created";
 	}
 }
